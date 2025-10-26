2cfa5109672d0f2ae0a95bb8e22e831c018447a1
christianlosamia <christianlosamia@gmail.com>
2025-10-26 16:41:51 +0800

    finalization of the UI and the layers

--- app/src/main/java/com/example/shuttleapp/MainActivity.kt
+++ app/src/main/java/com/example/shuttleapp/MainActivity.kt
@@ -1,49 +1,30 @@
- package com.example.shuttleapp
+package com.example.shuttleapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.shuttleapp.ui.theme.ShuttleAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
- @AndroidEntryPoint
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
-            ShuttleAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            Box(Modifier.Companion.systemBarsPadding()) {
+
+                ShuttleAppTheme {
+
+                    Navigation()
                 }
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun GreetingPreview() {
-    ShuttleAppTheme {
-        Greeting("Android")
     }
 }
