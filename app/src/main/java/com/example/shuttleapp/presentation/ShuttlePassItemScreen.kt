c9bbe192036310a6d7357bb71d2176ba67a3fcc6
christianlosamia <christianlosamia@gmail.com>
2025-10-26 16:32:09 +0800

    implement ShuttlePassItemScreen.kt and ShuttlePassListScreen.kt

--- app/src/main/java/com/example/shuttleapp/presentation/ShuttlePassItemScreen.kt
+++ app/src/main/java/com/example/shuttleapp/presentation/ShuttlePassItemScreen.kt
@@ -1,4 +1,285 @@
-package com.example.shuttleapp.presentation
+package com.example.shuttleapp.presentation//package com.example.shuttle.view.screen
 
-class ShuttlePassItemScreen {
+
+import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Cancel
+import androidx.compose.material.icons.outlined.CheckCircleOutline
+import androidx.compose.material.icons.outlined.Upload
+import androidx.compose.material.icons.rounded.CheckCircleOutline
+import androidx.compose.material.icons.twotone.Cancel
+import androidx.compose.material.icons.twotone.CheckCircle
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.example.shuttleapp.data.mappers.toDto
+import com.example.shuttleapp.presentation.components.InformationTextDialog
+import com.example.shuttleapp.presentation.components.LoadingDialog
+import com.example.shuttleapp.presentation.components.TopBarComponent
+import com.example.shuttleapp.presentation.viewmodel.ShuttleViewModel
+import com.example.shuttleapp.data.network.response.ShuttlePassWithPassengerDto
+import com.example.shuttleapp.presentation.state.UploadUiState
+import com.example.shuttleapp.ui.theme.Green
+import com.example.shuttleapp.ui.theme.Red
+import com.example.shuttleapp.ui.theme.White
+import com.example.shuttleapp.util.Resource
+import com.example.shuttleapp.util.displayMonthYear
+
+import kotlinx.coroutines.launch
+import java.sql.Time
+
+
+@Composable
+fun ShuttlePassItemScreen(id: String, viewModel: ShuttleViewModel = hiltViewModel(), navController: NavController) {
+
+    val shuttlePassWithPassengers by viewModel.shuttlePassWithPassengers.collectAsState()
+    val coroutineScope = rememberCoroutineScope()
+    val forUpdateShuttlePass by viewModel.forUpdateShuttlePass.collectAsState()
+
+    var uploadUiState by remember { mutableStateOf<UploadUiState>(UploadUiState.Idle) }
+    var uploadMessage by remember { mutableStateOf("") }
+
+    LaunchedEffect(id) {
+        viewModel.getShuttlePassWithPassengersById(id)
+        viewModel.loadShuttlePassById(id)
+    }
+
+    shuttlePassWithPassengers.data?.let { shuttlePassenger ->
+        Scaffold(
+            topBar = {
+                TopBarComponent(
+                    navController = navController,
+                    title = "SPT-${displayMonthYear()}-${shuttlePassenger.shuttlePass.id.takeLast(12).uppercase()}",
+                    action = {
+
+
+                        IconButton(
+                            onClick = {
+
+                                coroutineScope.launch{
+                                    try {
+
+                                        val postShuttleWithPassenger = shuttlePassenger.toDto()
+                                        val dtoList = listOf(postShuttleWithPassenger)
+                                        viewModel.postShuttlePassWithPassenger(dtoList)
+
+
+                                    } catch (ex: Exception) {
+                                        ex.printStackTrace()
+                                        Log.e("ShuttlePassWithPassenger", "error")
+                                    }
+
+                                }
+
+                            },
+                            enabled = !shuttlePassenger.shuttlePass.isSynced
+                        ) {
+                            Icon(
+                                imageVector = if(!shuttlePassenger.shuttlePass.isSynced) Icons.Outlined.Upload else Icons.TwoTone.CheckCircle,
+                                tint = if(!shuttlePassenger.shuttlePass.isSynced) White else Green,
+                                contentDescription = "Upload button")
+                        }
+
+                        LaunchedEffect(Unit) {
+                            //collect the response
+                            viewModel.postResult.collect { postResult ->
+                                when (postResult) {
+                                    is Resource.Error -> {
+                                        uploadUiState = UploadUiState.Error
+                                        postResult.message?.let { uploadMessage = it }
+                                    }
+                                    is Resource.Loading -> {
+                                        if (postResult.isLoading) {
+                                            uploadUiState = UploadUiState.Loading
+                                        }
+                                    }
+                                    is Resource.Success -> {
+                                        uploadUiState = UploadUiState.Success
+                                        postResult.data?.message?.let { uploadMessage = it }
+
+                                        val updateSync = listOf(forUpdateShuttlePass.copy(
+                                            isSynced = true
+                                        ))
+                                        viewModel.updateShuttlePass(updateSync)
+                                    }
+
+                                }
+                            }
+
+                        }
+
+                        when (uploadUiState) {
+                            is UploadUiState.Loading -> LoadingDialog()
+                            is UploadUiState.Success -> {
+                                InformationTextDialog(
+                                    onDismissRequest = { uploadUiState = UploadUiState.Idle },
+                                    dialogTitle = "Success!",
+                                    dialogText = uploadMessage,
+                                    icon = Icons.TwoTone.CheckCircle,
+                                    iconTint = Green
+                                )
+                            }
+                            is UploadUiState.Error -> {
+                                InformationTextDialog(
+                                    onDismissRequest = { uploadUiState = UploadUiState.Idle },
+                                    dialogTitle = "Error!",
+                                    dialogText = uploadMessage,
+                                    icon = Icons.TwoTone.Cancel,
+                                    iconTint = Red
+                                )
+                            }
+                            else -> {}
+                        }
+
+                    }
+                )
+            })
+        { innerPadding ->
+            Box(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .consumeWindowInsets(innerPadding)
+            ) {
+                Column {
+                    ShuttlePassFormTest(shuttlePassenger.shuttlePass, shuttlePassenger.passengersQR, viewModel, navController)
+                }
+
+            }
+        }
+    }
+
 }
+
+
+
+
+@Composable
+@Preview(showBackground = true)
+fun Prev(){
+    InformationTextDialog(
+        onDismissRequest = {},
+        dialogTitle = "Success",
+        dialogText = "The shuttle pass has been uploaded to the server.",
+        icon = Icons.Outlined.CheckCircleOutline,
+        iconTint = Green
+    )
+}
+
+
+
+//@Composable
+//fun ExpandedDropDown(label: String, itemsList: List<String>,value: String, errorIndicator: Boolean, onValueChanged: (String) -> Unit) {
+//
+//    var isExpanded by remember { mutableStateOf(false) }
+//
+//    ExposedDropdownMenuBox(
+//        expanded = isExpanded,
+//        onExpandedChange = { isExpanded = !isExpanded }
+//    ) {
+//        OutlinedTextField(
+//            modifier = Modifier.fillMaxWidth().menuAnchor(type = MenuAnchorType.PrimaryEditable, enabled = true),
+//            readOnly = true,
+//            value = value,
+//            onValueChange = {newValue ->
+//                onValueChanged(newValue)
+//            },
+//            label = { Text(label, color = Black) },
+//            enabled = errorIndicator,
+//            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded)},
+//            colors = OutlinedTextFieldDefaults.colors(),
+//            keyboardOptions = KeyboardOptions(
+//                keyboardType = KeyboardType.Text,
+//                imeAction = ImeAction.Next),
+//        )
+//
+//        ExposedDropdownMenu(modifier = Modifier.exposedDropdownSize().fillMaxWidth().heightIn(max = 230.dp).background(color = White),
+//            scrollState = rememberScrollState(), expanded = isExpanded, onDismissRequest = {isExpanded = false} //<--
+//        ) {
+//            if(itemsList.isNotEmpty()) {
+//                itemsList.forEach { item ->
+//                    DropdownMenuItem(
+//                        text = { Text(item, fontSize = 16.sp, color = Black) },
+//                        onClick = {
+//                            onValueChanged(item)
+//                            isExpanded = false
+//                        }
+//                    )
+//                }
+//            } else {
+//                Text("Empty. Having trouble while getting values", fontSize = 12.sp, modifier = Modifier.align(Alignment.CenterHorizontally))
+//
+//            }
+//
+//        }
+//    }
+//
+//
+//    AnimatedVisibility(visible = isExpanded) { // <-- only show when text field is clicked
+//        Card(
+//            modifier = Modifier
+//                .padding(horizontal = 5.dp)
+//                .width(textFieldSize.width.dp),
+//            elevation = CardDefaults.cardElevation(10.dp),
+//            colors = CardDefaults.cardColors(containerColor = White, contentColor = Black)
+//        ) {
+//            LazyColumn(
+//                modifier = Modifier.heightIn(max = 150.dp)
+//            ) {
+//                if (value.isNotEmpty()) {
+//                    items(
+//                        categories.filter {
+//                            it.lowercase()
+//                                .contains(value.lowercase())
+//                        }.sorted()
+//                    ) { item ->
+//                        Row(
+//                            modifier = Modifier
+//                                .fillMaxWidth()
+//                                .padding(12.dp)
+//                                .clickable {
+//                                    value = item
+//                                    isExpanded = false
+//                                    onValueChanged(item)
+//                                }
+//                        ) {
+//                            Text(text = item, fontSize = 16.sp)
+//                        }
+//                    }
+//                } else {
+//                    items(categories.sorted()) { item ->
+//                        Row(
+//                            modifier = Modifier
+//                                .fillMaxWidth()
+//                                .padding(12.dp)
+//                                .clickable {
+//                                    value = item
+//                                    isExpanded = false
+//                                    onValueChanged(item)
+//                                }
+//                        ) {
+//                            Text(text = item, fontSize = 16.sp)
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+//}
--- app/src/main/java/com/example/shuttleapp/presentation/ShuttlePassListScreen.kt
+++ app/src/main/java/com/example/shuttleapp/presentation/ShuttlePassListScreen.kt
@@ -1,4 +1,281 @@
 package com.example.shuttleapp.presentation
 
-class ShuttlePassListScreen {
+import android.annotation.SuppressLint
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Cancel
+import androidx.compose.material.icons.twotone.Cancel
+import androidx.compose.material.icons.twotone.CheckCircle
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.example.shuttleapp.data.mappers.toDto
+import com.example.shuttleapp.presentation.components.ActionDropdownMenu
+import com.example.shuttleapp.presentation.components.InformationTextDialog
+import com.example.shuttleapp.presentation.components.LoadingDialog
+import com.example.shuttleapp.presentation.components.StyledSnackBarHost
+import com.example.shuttleapp.ui.theme.White
+import com.example.shuttleapp.presentation.state.FilterType
+import com.example.shuttleapp.presentation.viewmodel.FilterViewModel
+import com.example.shuttleapp.presentation.components.TopBarComponent
+import com.example.shuttleapp.presentation.state.UploadUiState
+import com.example.shuttleapp.presentation.viewmodel.ShuttleViewModel
+import com.example.shuttleapp.ui.theme.Green
+import com.example.shuttleapp.ui.theme.Red
+import com.example.shuttleapp.util.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun ShuttlePassListScreen(id: String,
+                          shuttleViewModel: ShuttleViewModel = hiltViewModel(),
+                          filterViewModel: FilterViewModel = hiltViewModel(),
+                          navController: NavController) {
+
+    val filterState by filterViewModel.filterState.collectAsState()
+    val userState by shuttleViewModel.userState.collectAsState()
+    var uploadUiState by remember { mutableStateOf<UploadUiState>(UploadUiState.Idle) }
+    var uploadMessage by remember { mutableStateOf("") }
+    val coroutineScope = rememberCoroutineScope()
+    val snackBarHostState = remember { SnackbarHostState() }
+
+
+    LaunchedEffect(id) { //with param id para controlled
+        filterViewModel.loadShuttleWithPassenger(id)
+        Log.i("loadShuttleWithPassenger", "load shuttle pass with passenger done")
+    }
+
+    Scaffold(
+        snackbarHost = { StyledSnackBarHost(hostState = snackBarHostState) },
+        topBar = {
+            TopBarComponent(
+                title = "Completed Shuttle Pass List",
+                navController = navController,
+                action = {
+                    userState.userData?.let { userData ->
+
+                        ActionDropdownMenu(
+                            onUploadAll = {
+
+                                coroutineScope.launch {
+                                    val accountId = userState.userData?.accountId
+                                    if (accountId == null) {
+                                        snackBarHostState.showSnackbar("No account", "Dismiss", duration = SnackbarDuration.Short)
+                                        return@launch
+                                    }
+                                    val unsyncedItems = withContext(Dispatchers.IO) {
+                                        shuttleViewModel.getAllUnsyncedShuttlePasses(accountId)
+                                    }
+                                    if (unsyncedItems.isEmpty()) {
+                                        snackBarHostState.showSnackbar("No unsynced shuttle pass", "Dismiss", duration = SnackbarDuration.Short)
+                                        return@launch
+                                    }
+                                    // transform to DTOs and call viewmodel which will emit events
+                                    shuttleViewModel.postShuttlePassWithPassenger(unsyncedItems.map { it.toDto() })
+                                }
+                            },
+                            onDeleteAll = {
+                                coroutineScope.launch {
+                                    snackBarHostState.showSnackbar(
+                                        message = "Delete not authorized",
+                                        actionLabel = "Dismiss",
+                                        withDismissAction = true,
+                                        duration = SnackbarDuration.Short
+                                    )
+                                }
+                            }
+                        )
+                    }
+
+                }
+            )
+        }
+
+    ) { innerPadding ->
+
+        Box(modifier = Modifier
+            .padding(innerPadding)
+            .consumeWindowInsets(innerPadding)
+            .fillMaxSize()) {
+
+            Column(
+
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(White),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+
+            ) {
+                userState.userData?.let { userData ->
+                    FilterChoiceSegmentedButton(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp, vertical = 8.dp),
+                        selectedFilter = filterState.currentFilter,
+                        onFilterSelected = {filter ->
+                            when(filter) {
+                                FilterType.All -> {
+                                    filterViewModel.loadShuttleWithPassenger(userData.accountId)
+                                }
+                                FilterType.Late -> {
+                                    filterViewModel.loadShuttleWithPassengerFilterLate(userData.accountId)
+                                }
+                                FilterType.NotSynced -> {
+                                    filterViewModel.loadShuttleWithPassengerFilterUnsynced(userData.accountId)
+
+                                }
+                            }
+
+                        },
+                    )
+                }
+
+                HorizontalDivider(Modifier.fillMaxWidth())
+
+                when {
+                    filterState.list.isEmpty() -> {
+                        Spacer(Modifier.weight(0.10f))
+                        Text(text = "No items to display",
+                            fontStyle = FontStyle.Italic,
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        )
+                        Spacer(Modifier.weight(0.90f))
+                    }
+                    else -> {
+                        LazyColumn(
+                            modifier = Modifier
+                                .background(color = White)
+                                .padding(8.dp)
+                                .fillMaxWidth())
+                        {
+
+                            //display all the shuttle pass items here
+                            items(filterState.list.reversed())  {item ->
+                                ShuttlePassListItem(item, navController)
+                            }
+
+
+                        }
+                    }
+                }
+            }
+
+            LaunchedEffect(Unit) {
+                shuttleViewModel.postResult.collect{ postResult ->
+                    when (postResult) {
+                        is Resource.Error -> {
+                            uploadUiState = UploadUiState.Error
+                            postResult.message?.let {uploadMessage = it  }
+                        }
+                        is Resource.Loading -> {
+                            if (postResult.isLoading) {
+                                uploadUiState = UploadUiState.Loading
+                            }
+                        }
+                        is Resource.Success -> {
+                            uploadUiState = UploadUiState.Success
+                            postResult.data?.message?.let { uploadMessage = it }
+
+                            val unsyncedItems = userState.userData?.let { shuttleViewModel.getAllUnsyncedShuttlePasses(it.accountId) }
+                            val updateListItem = unsyncedItems!!.map { item -> item.shuttlePass.copy(isSynced = true)}
+                            shuttleViewModel.updateShuttlePass(updateListItem)
+                        }
+
+                    }
+                }
+            }
+
+            when (uploadUiState) {
+                is UploadUiState.Loading -> LoadingDialog()
+                is UploadUiState.Success -> {
+                    InformationTextDialog(
+                        onDismissRequest = { uploadUiState = UploadUiState.Idle },
+                        dialogTitle = "Success!",
+                        dialogText = uploadMessage,
+                        icon = Icons.TwoTone.CheckCircle,
+                        iconTint = Green
+                    )
+                }
+                is UploadUiState.Error -> {
+                    InformationTextDialog(
+                        onDismissRequest = { uploadUiState = UploadUiState.Idle },
+                        dialogTitle = "Error!",
+                        dialogText = uploadMessage,
+                        icon = Icons.TwoTone.Cancel,
+                        iconTint = Red
+                    )
+                }
+                else -> {}
+            }
+
+        }
+    }
+}
+
+@Composable
+private fun FilterChoiceSegmentedButton(
+    modifier: Modifier = Modifier,
+    selectedFilter: FilterType,
+    onFilterSelected: (FilterType) -> Unit
+) {
+
+    val options = listOf(
+        "All" to FilterType.All,
+        "Late" to FilterType.Late,
+        "Not Synced" to FilterType.NotSynced
+    )
+
+    val selectedIndex = options.indexOfFirst { it.second == selectedFilter }
+
+    SingleChoiceSegmentedButtonRow(modifier = modifier) {
+        options.forEachIndexed { index, (label, filterType) ->
+            SegmentedButton(
+                shape = SegmentedButtonDefaults.itemShape(
+                    index = index,
+                    count = options.size
+                ),
+                onClick = {
+                    onFilterSelected(filterType)
+                },
+                selected = index == selectedIndex,
+                label = { Text(label) }
+            )
+        }
+    }
 }
